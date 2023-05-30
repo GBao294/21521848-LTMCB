26f2d3fd36408e704dfef6cc6a453f520f73129e
GBao294 <105119912+GBao294@users.noreply.github.com>
2023-05-30 14:32:15 +0700

    Add files via upload

---
+++ Lab4 1+2+4+5/Lab4/Bai3.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Lab4
+{
+    partial class Bai3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            textBox1 = new TextBox();
+            pnlWeb = new Panel();
+            label2 = new Label();
+            fileurl = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 40);
+            button1.TabIndex = 0;
+            button1.Text = "AddBrowser";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(134, 0);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 40);
+            button2.TabIndex = 1;
+            button2.Text = "Go";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(4, 61);
+            button3.Name = "button3";
+            button3.Size = new Size(116, 40);
+            button3.TabIndex = 2;
+            button3.Text = "Download";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(134, 61);
+            button4.Name = "button4";
+            button4.Size = new Size(116, 40);
+            button4.TabIndex = 3;
+            button4.Text = "DownloadResource";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(273, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(515, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // pnlWeb
+            // 
+            pnlWeb.Location = new Point(4, 107);
+            pnlWeb.Name = "pnlWeb";
+            pnlWeb.Size = new Size(1920, 1080);
+            pnlWeb.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(807, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 25);
+            label2.TabIndex = 10;
+            label2.Text = "FileURL";
+            // 
+            // fileurl
+            // 
+            fileurl.Location = new Point(276, 68);
+            fileurl.Name = "fileurl";
+            fileurl.Size = new Size(512, 27);
+            fileurl.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(807, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 25);
+            label1.TabIndex = 11;
+            label1.Text = "URL";
+            // 
+            // Bai3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(897, 450);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(fileurl);
+            Controls.Add(pnlWeb);
+            Controls.Add(textBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Bai3";
+            Text = "Bai3";
+            Load += Bai3_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+        private Panel pnlWeb;
+        private Label label2;
+        private TextBox fileurl;
+        private Label label1;
+    }
+}
---
+++ Lab4 1+2+4+5/Lab4Bai3/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Lab4Bai3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(172, 42);
+            this.browser.TabIndex = 0;
+            this.browser.Text = "Browser";
+            this.browser.UseVisualStyleBackColor = true;
+            this.browser.Click += new System.EventHandler(this.browser_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(205, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(694, 28);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(413, 48);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 493);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.browser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browser;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
